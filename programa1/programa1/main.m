22b7b5cfa4e9713fdcd07fb3f9d9a665968b234a
darioalessandro <darioalessandrolencina@gmail.com>
2012-09-03 13:04:53 -0400

    testing
    testing

---
+++ programa1/programa1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  programa1
+//
+//  Created by Dario Lencina on 9/3/12.
+//  Copyright (c) 2012 Dario Lencina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
