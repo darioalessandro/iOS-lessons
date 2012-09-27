90d1b66b6a6ca7abccf79cd0703181e590a493f9
Romina <romina23_vip@hotmail.com>
2012-09-27 14:54:35 -0500

    Iphone 1st app

---
+++ IOS Apps/Hello World/Hello World/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Hello World
+//
+//  Created by 23 * Romanovski * 23 on 27/09/12.
+//  Copyright (c) 2012 com.Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ IOS Apps/Hello World/Hello World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by 23 * Romanovski * 23 on 27/09/12.
+//  Copyright (c) 2012 com.Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
