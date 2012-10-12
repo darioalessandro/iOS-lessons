e75139390671a77927db1488b06942631c3d9763
Romina <romina23_vip@hotmail.com>
2012-10-11 20:19:23 -0500

    Multiview app

--- IOS Apps/View  Switcher/View  Switcher/BIDSwitchViewController.h
+++ IOS Apps/View  Switcher/View  Switcher/BIDSwitchViewController.h
@@ -7,13 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+
 @class BIDYellowViewController;
 @class BIDBlueViewController;
 
 
 
-@interface BIDSwitchViewController : UIView
+@interface BIDSwitchViewController : UIViewController
     @property (strong, nonatomic) BIDYellowViewController *yellowViewController;
-    @property (strong, nonatomic) BIDBlueViewController *blueViewController;
+    @property (strong, nonatomic)
+        BIDBlueViewController *blueViewController;
     - (IBAction)switchViews:(id)sender;
 @end
