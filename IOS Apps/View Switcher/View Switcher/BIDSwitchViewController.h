54011956c41a94a2ecdb117c584fd350966a2cc0
Romina <romina23_vip@hotmail.com>
2012-10-15 14:46:06 -0500

    finished app

--- IOS Apps/View Switcher/View Switcher/BIDSwitchViewController.h
+++ IOS Apps/View Switcher/View Switcher/BIDSwitchViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+@class BIDYellowViewController;
+@class BIDBlueViewController;
 
 @interface BIDSwitchViewController : UIViewController
+
+@property (strong, nonatomic) BIDYellowViewController *yellowViewController; @property (strong, nonatomic) BIDBlueViewController *blueViewController;
+- (IBAction)switchViews:(id)sender;
 
 @end
--- IOS Apps/View Switcher/View Switcher/BIDSwitchViewController.m
+++ IOS Apps/View Switcher/View Switcher/BIDSwitchViewController.m
@@ -7,12 +7,17 @@
 //
 
 #import "BIDSwitchViewController.h"
+#import "BIDYellowViewController.h" 
+#import "BIDBlueViewController.h"
 
 @interface BIDSwitchViewController ()
 
 @end
 
 @implementation BIDSwitchViewController
+
+@synthesize yellowViewController;
+@synthesize blueViewController;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -25,14 +30,46 @@
 
 - (void)viewDidLoad
 {
+    
+    self.blueViewController = [[BIDBlueViewController alloc] initWithNibName:
+    @"BlueView" bundle:nil];
+    [self.view insertSubview:self.blueViewController.view atIndex:0];
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+	}
+- (IBAction)switchViews:(id)sender {
+    [UIView beginAnimations:@"View Flip" context:nil];
+    [UIView setAnimationDuration:4.0];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+    if (self.yellowViewController.view.superview == nil) { if (self.yellowViewController == nil) {
+        self.yellowViewController =
+        [[BIDYellowViewController alloc] initWithNibName:@"YellowView"
+                                                  bundle:nil];
+    }
+        [UIView setAnimationTransition:
+         UIViewAnimationTransitionCurlUp forView:self.view cache:YES];
+        [self.blueViewController.view removeFromSuperview];
+        [self.view insertSubview:self.yellowViewController.view atIndex:0]; } else {
+            if (self.blueViewController == nil) {
+                self.blueViewController =
+                [[BIDBlueViewController alloc] initWithNibName:@"BlueView"
+                                                        bundle:nil];
+            }
+            [UIView setAnimationTransition:
+            UIViewAnimationTransitionCurlDown forView:self.view cache:YES];
+            [self.yellowViewController.view removeFromSuperview];
+            [self.view insertSubview:self.blueViewController.view atIndex:0]; }
+    [UIView commitAnimations];
 }
 
 - (void)didReceiveMemoryWarning
 {
+    if (self.blueViewController.view.superview == nil) { self.blueViewController = nil;
+    } else {
+        self.yellowViewController = nil;
+    }
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
